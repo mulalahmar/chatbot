452cd521f16be5d995c15d1e3b89a58795ab46e9
Maher <7259510+mulalahmar@users.noreply.github.com>
2024-11-03 04:09:18 +0000

    First update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# 💬 Chatbot template
+# 🫖 Teapot
 
-A simple Streamlit app that shows how to build a chatbot using OpenAI's GPT-3.5.
+Optimization Assistant is a chatbot that uses Artificial Intelligence to understand, model and solve optimization problems. This particular example solves for a simple inear program End-to-End.
 
 [![Open in Streamlit](https://static.streamlit.io/badges/streamlit_badge_black_white.svg)](https://chatbot-template.streamlit.app/)
 
--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,9 @@ import streamlit as st
 from openai import OpenAI
 
 # Show title and description.
-st.title("💬 Chatbot")
+st.title(":teapot: Teapot.. Solving Your Most Complex Optimization Problems")
 st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
+    "This is your Optimization Assistant, a chatbot that uses Artificial Intelligence to understand, model and solve optimization problems. This particular example solves for a simple inear program End-to-End. "
     "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
     "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
 )
@@ -32,7 +32,7 @@ else:
 
     # Create a chat input field to allow the user to enter a message. This will display
     # automatically at the bottom of the page.
-    if prompt := st.chat_input("What is up?"):
+    if prompt := st.chat_input("How can I help you?"):
 
         # Store and display the current prompt.
         st.session_state.messages.append({"role": "user", "content": prompt})
